37afa9c6671bd4b1a138e1786ff3b28b33d996e0
Jamie Barton <hi@jamiebarton.co.uk>
2018-03-15 10:56:50 +0000

    Add example handleBttn func

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const { json } = require('micro');
+const { json } = require('micro')
+const fetch = require('node-fetch');
+
+const { BTTN_API_KEY } = process.env;
+
+const handleBttn = async callback => {
+  const response = await fetch(callback, {
+    method: 'POST',
+    headers: {
+      'X-Api-Key': BTTN_API_KEY
+    },
+    body: JSON.stringify({
+      result: 'success'
+    })
+  })
+
+  return await response.json()
+}
 
 module.exports = async (req, res) => {
-  const { callback } = await json(req);
+  const { callback = undefined } = await json(req)
 
   if (!callback) {
-    // Error
+    console.error('No callback URL provided.');
   }
 
+  const bttn = await handleBttn(callback);
+
   return {
-    success: true
-  };
-};
+    result: 'success'
+  }
+}
